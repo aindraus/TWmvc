97c293139bcbf805dc270a6bf452a16280ec8cfd
Anthony I <aindraus@gmail.com>
2017-12-13 14:23:32 +1100

    scaffolding v0.1 -NC

---
+++ app/config/scaffold.php
@@ -0,0 +1,37 @@
+<?php
+// Create New Scaffold - Create new Model, Controller, DB Table, Views (index, edit, add, show)
+class Scaffold {
+  private $name;
+  private $model;
+  private $dbarray = [];
+
+  public function __construct($name) {
+    $this->name = $name;
+    if(!empty($name)) {
+        $sql = 'CREATE TABLE IF NOT EXISTS tw_' . $name . ' (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)';
+        // $sql = 'INSERT INTO users(title) VALUES (:title)';
+        $host = 'localhost';
+        $user = 'root';
+        $password = 'root';
+        $db = 'pdotest1';
+        // Set DSN
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $db;
+        //SET DATABASE VARIABLES
+        $pdo = new PDO($dsn, $user, $password);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        if(!$stmt) {
+          die('Error');
+        }
+        // Creating Required Files
+        mkdir('../../app/views/' . $name, 0777);
+        $fileCreate = ['index', 'edit', 'add', 'show'];
+        foreach($fileCreate as $file) {
+          $myFile = touch('../../app/views/' . $name . '/' . $file . '.php');
+        }
+    } else {
+      echo "error with name";
+    }
+
+  }
+}
---
+++ public/scaffold/create.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../../app/bootstrap.php';
+require_once APP_ROOT . '/config/scaffold.php';
+
+$posts = new Scaffold('posts');
+
+// echo $posts->name;
